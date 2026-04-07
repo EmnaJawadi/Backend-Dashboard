c603524a367fd1cf1b23dbf81f8ec4163897c812
Emna Jawadi <jawadiamna18@gmail.com>
2026-04-07 13:41:37 +0100

    suppresion des dossiers doublons

--- src/modules/users/dto/user-query.dto.ts
+++
@@ -1,9 +0,0 @@
-import { UserRole } from '../entities/user.entity';
-
-export class UserQueryDto {
-  search?: string;
-  role?: UserRole;
-  isActive?: string;
-  page?: number;
-  limit?: number;
-}
